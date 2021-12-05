7ac309b9a3930d63dadf874524f3550065bc9824
Kathleen Doviken <kathleendoviken@gmail.com>
2021-12-04 18:43:30 -0600

    not showing up pls fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  HashRouter,
+  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";import ReactDOM from 'react-dom';
@@ -10,12 +10,12 @@ import reportWebVitals from './reportWebVitals';
 import QualifyingTemplate from './components/QualifyingTemplate';
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
       <Route path="/" element={<App />} />
       <Route path="/quali" element={<QualifyingTemplate />} />
     </Routes>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
